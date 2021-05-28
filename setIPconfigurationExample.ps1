46f5d2e6f3e4e462dd4c6e1903f6ac594205b77e
oliver@revilo.com.au <oliver@revilo.com.au>
2021-05-28 10:21:14 +1000

    commit all

--- setIPconfigurationExample.ps1
+++ setIPconfigurationExample.ps1
@@ -1,7 +1,12 @@
+#set IP settig for interface $adapter
 $ip = "192.168.100.10"
 $prefix = "24"
 $GW = "192.168.100.1"
-$DNS = "192.168.100.10"
-$adapter = (Get-NetAdapter).ifIndex
+$adapter = (Get-NetAdapter).ifIndex #won't work with multiple adapters...
 New-NetIPAddress -IPAddress $ip -PrefixLength $prefix `
 -InterfaceIndex $adapter -DefaultGateway $GW
+
+#set DNS for interface 12 < find this value with Get-NetAdapter
+$DNS1 = "192.168.100.10"
+$DNS2 = "192.168.100.11"
+Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses ($DNS1,$DNS2)
