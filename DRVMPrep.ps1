8a43f97030f1645bca6187b24b0749bc572d603c
oliver@revilo.com.au <oliver@revilo.com.au>
2021-05-28 10:05:48 +1000

    some ps scripts

---
+++ DRVMPrep.ps1
@@ -0,0 +1,7 @@
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
+Set-TimeZone "E. Australia Standard Time"
+Set-WinSystemLocale -SystemLocal en-AU
+$url = "http://cdn.cloudbackup.management/maxdownloads/mxb-rc-windows-x64.exe"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -uri $url -OutFile rc.exe
+Restart-Computer
