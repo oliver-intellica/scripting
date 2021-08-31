e2d7cabfd7242f0f3d0aaaba908ee572941e8c25
Oliver Caldwell <oliver@revilo.com.au>
2021-09-01 09:24:40 +1000

    Update security.ps1

--- security.ps1
+++ security.ps1
@@ -1,2 +1,2 @@
-// Display a tag on inbound external emails in outlook
+// Display a tag on inbound external emails in outlook - increases likelihood of end user avoid phishing/scams email attacks
 Set-ExternalInOutlook -Enabled $true
