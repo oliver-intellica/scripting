014f9b4f9bd97c6fc6e9b33a48e11a74ead64489
Oliver Caldwell <oliver@revilo.com.au>
2022-03-23 15:25:52 +1000

    Update and rename extract-urls.bash to extract-urls.sh

--- extract-urls.sh
+++ extract-urls.sh
@@ -1,2 +1,2 @@
-# Extracts and prints to STDOUT unique URLS of links to the same site.
+# Extracts and prints to STDOUT unique URLS of links to the same site using curl, grep and sed.
 curl https://www.example.com | grep "https://www.example.com" | sed 's/https/\nhttps/g' | grep ^https://www.example.com |  sed 's/\(^http[^"]\)\(.\)/\1/g' | grep http | sort -u
