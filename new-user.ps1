158a63b7906ea6c5f6de461954b79c16e3d0c665
Oliver Caldwell <oliver@revilo.com.au>
2022-04-08 09:47:11 +1000

    Update new-user.ps1

--- new-user.ps1
+++ new-user.ps1
@@ -21,7 +21,7 @@ start-AdsyncSyncCycle -PolicyType Delta
 $migrationEndpointOnPrem = get-migrationendpoint instead of creating a new one.
 
 #Creation the batch and autostart and autocomplete it
-$OnboardingBatch = New-MigrationBatch -Name RemoteOnBoarding1 -SourceEndpoint $MigrationEndpointOnprem.Identity -TargetDeliveryDomain contoso.mail.onmicrosoft.com -CSVData ([System.IO.File]::ReadAllBytes("C:\Users\Administrator\Desktop\RemoteOnBoarding1.csv"))  -AutoComplete -AutoStart -NotificationEmails oliver@intellica.com.a
+$OnboardingBatch = New-MigrationBatch -Name RemoteOnBoarding1 -SourceEndpoint $MigrationEndpointOnprem.Identity -TargetDeliveryDomain contoso.mail.onmicrosoft.com -CSVData ([System.IO.File]::ReadAllBytes("C:\Users\Administrator\Desktop\RemoteOnBoarding1.csv"))  -AutoComplete -AutoStart -NotificationEmails user@user.com
 Start-MigrationBatch -Identity $OnboardingBatch.Identity # only needed if autstart not specified in New-MigraitonBatch
 
 ## Note the -CSVDATA file should be in the format:
