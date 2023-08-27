7298e762b458d3c136e011ad09816ba9dca4d260
Oliver Caldwell <oliver@revilo.com.au>
2023-08-28 09:33:03 +1000

    Update set time.cmd

--- set time.cmd
+++ set time.cmd
@@ -10,3 +10,6 @@ w32tm /config /reliable:yes
 REMARK Start the w32time service:
 net start w32time
 
+REMARK Wait ~1 minute and recheck that the config has stuck and the time has synced succesfully to the new source
+w32tm /query /status
+
