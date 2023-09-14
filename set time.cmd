3ce8a4cdc466090378baf824dbf5d24a21001199
Oliver Caldwell <oliver@revilo.com.au>
2023-09-15 08:44:12 +1000

    Update set time.cmd

--- set time.cmd
+++ set time.cmd
@@ -1,18 +1,18 @@
 REM lets set some reliable time sources - good for PDCs on Windows Domains and Hyper-V hosts.
 REM Author Oliver Caldwell; 28 August 2023.
 
-REMARK Stop the W32Time service:
+REM Stop the W32Time service:
 net stop w32time
 
-REMARK Configure the external time sources. I like time.nist.gov but the pool.ntp.org project also a valid choice.
+REM Configure the external time sources. I like time.nist.gov but the pool.ntp.org project also a valid choice.
 w32tm /config /syncfromflags:manual /manualpeerlist:time.nist.gov
 
-REMARK Make your PDC a reliable time source for the clients:
+REM Make your PDC a reliable time source for the clients:
 w32tm /config /reliable:yes
 
-REMARK Start the w32time service:
+REM Start the w32time service:
 net start w32time
 
-REMARK Wait ~1 minute and recheck that the config has stuck and the time has synced succesfully to the new source
+REM Wait ~1 minute and recheck that the config has stuck and the time has synced succesfully to the new source
 w32tm /query /status
 
